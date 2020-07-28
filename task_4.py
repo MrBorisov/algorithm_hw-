25ac335f512d6d135da7fd452677a3a9df80188b
mrborisov <mrborisovv@gmail.com>
2020-07-29 07:40:03 +1000

    add teacher res and 4 task

--- task_4.py
+++ task_4.py
@@ -9,3 +9,21 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def summ_of_elem(counter, num=1, sum_els=0):
+    if counter > 0:
+        counter -= 1
+        sum_els = sum_els + num
+        num = num / 2
+        return summ_of_elem(counter, num, sum_els)
+    else:
+        print(sum_els)
+        return sum_els
+
+
+try:
+    numb = int(input('введите число: '))
+except ValueError:
+    print('вы ввели не число')
+print(f'сумма = {summ_of_elem(numb)}')
