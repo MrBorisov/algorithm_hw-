275d8873b9a408435ff88bc54787069d44862895
mrborisov <mrborisovv@gmail.com>
2020-08-25 09:31:18 +1000

    chenge 3

--- task_3.py
+++ task_3.py
@@ -18,31 +18,25 @@ import copy
 from statistics import median
 
 def find_mediana(unsrt_lst):
-    i = 0
-    for i in range(len(unsrt_lst)):
-        unsrt_lst_copy = copy.copy(unsrt_lst)
-        left_lst = []
-        right_lst = []
-        my_mediana = unsrt_lst_copy.pop(i)
-        for el in unsrt_lst_copy:
-            if el > my_mediana:
-                right_lst.append(el)
-            elif el <= my_mediana:
-                left_lst.append(el)
-
-
-        if len(left_lst) == len(right_lst) and max(left_lst) <= my_mediana and min(right_lst) >= my_mediana:
-            break
-        else:
-            i +=1
-    return my_mediana
+    left_lst = []
+    right_lst = []
+    temp = unsrt_lst
+    for i in range(len(temp)):
+        for j in range(len(temp)):
+            if temp[i] > temp[j]:
+                left_lst.append(temp[j])
+            if temp[i] < temp[j]:
+                right_lst.append(temp[j])
+            if temp[i] == temp[j] and i > j:
+                left_lst.append(temp[j])
+            if temp[i] == temp[j] and i < j:
+                right_lst.append(temp[j])
+        if len(left_lst) == len(right_lst):
+            return temp[i]
+        left_lst.clear()
+        right_lst.clear()
 
 m = 3
-'''unsrt_lst = [random.randint(0, 100) for i in range(m * 2 + 1)]
-print(unsrt_lst)
-print(sorted(unsrt_lst))
-print(find_mediana(unsrt_lst))
-print(median(unsrt_lst))'''
 
 for i in range(100):
     unsrt_lst = [random.randint(0, 100) for i in range(m * 2 + 1)]
